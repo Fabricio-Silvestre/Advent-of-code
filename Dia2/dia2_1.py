7c13952c78deffde6077020f0cefcedbeccf1d9d
Fabricio Silvestre <fabricio.amorimfsilvestre@gmail.com>
2024-12-11 14:38:46 -0300

    Código desafio 1 - dia 2
    Upload da primeira versão do desafio 1 do dia 2 do Advent of code.

---
+++ Dia2/dia2_1.py
@@ -0,0 +1,10 @@
+with open('input2.txt','r') as arquivo:
+    lista_arquivo = arquivo.readlines()
+
+    listalinha=[]
+    contador = 0
+    for item in lista_arquivo:
+        listalinha.append(item.split(' '))
+        for itemlista in listalinha:
+            if 
+                
