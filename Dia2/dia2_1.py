8fd9f1da7a3862b65cac5ee597c84a997bd5e15a
Fabricio Silvestre <fabricio.amorimfsilvestre@gmail.com>
2024-12-18 17:25:24 -0300

    Update dia2_1.py
    Desafio 1 - Dia 2

--- Dia2/dia2_1.py
+++ Dia2/dia2_1.py
@@ -1,2 +1,19 @@
-with open('input2.txt','r') as arquivo:
+def verificar_seguranca(niveis):
+    crescente = all(niveis[i] < niveis[i + 1] and 1 <= niveis[i + 1] - niveis[i] <= 3 for i in range(len(niveis) - 1))
+    decrescente = all(niveis[i] > niveis[i + 1] and 1 <= niveis[i] - niveis[i + 1] <= 3 for i in range(len(niveis) - 1))
+    return crescente or decrescente
+
+with open('input2.txt', 'r') as arquivo:
     lista_arquivo = arquivo.readlines()
+
+    
+relatorios = []
+for linha in lista_arquivo:
+    niveis = list(map(int, linha.strip().split()))
+    relatorios.append(niveis)
+
+relatorios_seguros = 0
+for relatorio in relatorios:
+    relatorios_seguros += verificar_seguranca(relatorio)
+print(f"Total de relatórios seguros: {relatorios_seguros}")
+
